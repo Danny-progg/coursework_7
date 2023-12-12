13b484c59afc41341ba7cc352548e4ed405a4668
Danny-progg <122675148+Danny-progg@users.noreply.github.com>
2023-12-13 00:45:21 +0300

    test fixed

--- atomichabits/tests.py
+++ atomichabits/tests.py
@@ -75,39 +75,86 @@ class HabitTestCase(APITestCase):
             {"frequency": ["Нельзя выполнять привычку реже, чем 1 раз в 7 дней."]},
         )
 
+    # def test_related_habit_validation(self):
+    #     self.habit = Habit.objects.create(place="uuu", action="uuu", is_pleasant=False)
+    #
+    #     data = {"place": "home", "action": "do something", "related_habit": "2"}
+    #
+    #     response = self.client.post(reverse("atomichabits:habit_create"), data=data)
+    #
+    #     self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+    #
+    #     self.assertEqual(
+    #         response.json(),
+    #         {"related_habit": ["Связанная привычка должна быть полезной."]},
+    #     )
+
     def test_related_habit_validation(self):
         self.habit = Habit.objects.create(place="uuu", action="uuu", is_pleasant=False)
 
-        data = {"place": "home", "action": "do something", "related_habit": "2"}
+        data = {"place": "home", "action": "do something", "related_habit": 2}
 
         response = self.client.post(reverse("atomichabits:habit_create"), data=data)
 
-        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        errors = response.json().get("non_field_errors", [])
+
+        # Проверяем, что список ошибок содержит одну ошибку
+
+        self.assertEqual(len(errors), 1)
+
+        # Сравниваем первую ошибку в списке
 
         self.assertEqual(
-            response.json(),
-            {"related_habit": ["Связанная привычка должна быть полезной."]},
+            errors[0],
+            "Связанная привычка должна быть полезной, если привычка неприятная.",
         )
+
+
+    # def test_rel_habit_and_award_validation(self):
+    #     data = {
+    #         "place": "test_rel_habit_and_award_validation",
+    #         "action": "do something",
+    #         "award": "eat something",
+    #         "related_habit": "1"
+    #     }
+    #
+    #     response = self.client.post(reverse("atomichabits:habit_create"), data=data)
+    #
+    #     self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+    #
+    #     self.assertEqual(
+    #         response.json(),
+    #         {
+    #             "non_field_errors": [
+    #                 "Связанную привычку и вознаграждение нельзя выбирать одновременно"
+    #             ]
+    #         },
+    #     )
 
     def test_rel_habit_and_award_validation(self):
         data = {
             "place": "test_rel_habit_and_award_validation",
             "action": "do something",
             "award": "eat something",
-            "related_habit": "1"
+            "related_habit": 1,
         }
 
         response = self.client.post(reverse("atomichabits:habit_create"), data=data)
 
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
 
+        errors = response.json().get("non_field_errors", [])
+
+        # Проверяем, что список ошибок содержит одну ошибку
+
+        self.assertEqual(len(errors), 1)
+
+        # Сравниваем первую ошибку в списке
+
         self.assertEqual(
-            response.json(),
-            {
-                "non_field_errors": [
-                    "Связанную привычку и вознаграждение нельзя выбирать одновременно"
-                ]
-            },
+            errors[0],
+            "Недопустимый первичный ключ "
+            f"'{data['related_habit']}' - объект не существует.",
         )
 
     def test_is_pl_and_award_or_rel_h_validation(self):
